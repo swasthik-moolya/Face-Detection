7118a3f6bfbd2d16b228f50813fb877dbc60a1d4
Swasthik Moolya <153975788+swasthik-moolya@users.noreply.github.com>
2024-02-25 23:47:41 +0530

    Add files via upload

---
+++ face.py
@@ -0,0 +1,32 @@
+import cv2
+
+# Load the pre-trained face cascade classifier
+face_Cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read a frame from the webcam
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the grayscale frame
+    faces = face_Cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+
+    # Draw rectangles around the detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    # Display the frame with detected faces
+    cv2.imshow('Face Detection', frame)
+
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam and close all OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
